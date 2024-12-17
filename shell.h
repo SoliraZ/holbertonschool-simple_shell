c43dc9aac3b36ef88f811e848618047465d1a18b
Saru3450 <9971@holbertonstudents.com>
2024-12-17 14:02:37 +0000

    shell.h

---
+++ shell.h
@@ -0,0 +1,13 @@
+#ifndef SHELL_FILE
+#define SHELL_FILE
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+#endif
