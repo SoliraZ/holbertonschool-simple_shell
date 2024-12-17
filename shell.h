a6134ee7a777bbe5b0a06b636e4d00ff7172f6a1
SoliraZ <48773711+SoliraZ@users.noreply.github.com>
2024-12-17 15:01:32 +0100

    created shell.h header

---
+++ shell.h
@@ -0,0 +1,12 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+
+#endif
