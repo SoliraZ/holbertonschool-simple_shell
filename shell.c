d92b1d2bd90a966bf6ac14c0bd9a9afbf760e12a
SoliraZ <48773711+SoliraZ@users.noreply.github.com>
2024-12-19 11:26:57 +0100

    edited the shell.c main to have only the main inside and split the shell.c file with all the new function

--- shell.c
+++ shell.c
@@ -1,65 +1,6 @@
 #include "shell.h"
 
 /**
- * print_prompt - prints prompt
- */
-
-void	print_prompt(void)
-{
-	if (isatty(STDIN_FILENO) == 1)
-	{
-		printf("($) ");
-		fflush(stdin);
-	}
-}
-/**
- * tokenize - tokenizes user input
- * @user_input: user input
- *
- * Return: tokenized user input
- */
-
-char	**tokenize(char *user_input)
-{
-	char **args = NULL;
-	char *token;
-	int i = 0;
-
-	args = malloc(sizeof(char *) * 64);
-	if (args == NULL)
-	{
-		perror("malloc error");
-		exit(EXIT_FAILURE);
-	}
-	token = strtok(user_input, " ");
-	while (token != NULL)
-	{
-		args[i++] = token;
-		token = strtok(NULL, " ");
-	}
-	args[i] = '\0';
-	return (args);
-}
-/**
- * read_line - reads a line from stdin
- *
- * Return: user input
- */
-
-char	*read_line(void)
-{
-	char *user_input = NULL;
-	size_t size_alloc = 0;
-
-	if (getline(&user_input, &size_alloc, stdin) == -1)
-	{
-		perror("getline error");
-		exit(EXIT_FAILURE);
-	}
-	user_input[strcspn(user_input, "\n")] = '\0';
-	return (user_input);
-}
-/**
  * main - entry point
  *
  * Return: 0 on success, 1 on failure
