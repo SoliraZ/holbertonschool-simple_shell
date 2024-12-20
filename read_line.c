57c648bf0613edc330367599cec345996e19acfa
SoliraZ <48773711+SoliraZ@users.noreply.github.com>
2024-12-20 20:37:23 +0100

    refreshed all files to the correct ones

--- read_line.c
+++ read_line.c
@@ -10,11 +10,28 @@ char	*read_line(void)
 {
 	char *user_input = NULL;
 	size_t size_alloc = 0;
+	int res_getline;
 
-	if (getline(&user_input, &size_alloc, stdin) == -1)
+	res_getline = getline(&user_input, &size_alloc, stdin);
+	if (res_getline == EOF)
 	{
-		perror("getline error");
-		exit(EXIT_FAILURE);
+		free(user_input);
+		if (isatty(STDIN_FILENO))
+			putchar('\n');
+		exit(EXIT_SUCCESS);
+	}
+	if (res_getline == -1)
+	{
+		if (feof(stdin))
+		{
+			free(user_input);
+			exit(EXIT_SUCCESS);
+		}
+		else
+		{
+			perror("getline error");
+			exit(EXIT_FAILURE);
+		}
 	}
 	user_input[strcspn(user_input, "\n")] = '\0';
 	return (user_input);
--- shell.c
+++ shell.c
@@ -16,8 +16,8 @@ int main(void)
 		print_prompt();
 		user_input = read_line();
 		args = tokenize(user_input);
-		if (args[0] != NULL && (strcmp(args[0], "exit") == 0
-					|| strcmp(args[0], "env") == 0 || strcmp(args[0], "test") == 0))
+		if (strcmp(args[0], "exit") == 0
+				|| strcmp(args[0], "env") == 0 || strcmp(args[0], "test") == 0)
 			command(args, user_input);
 		else
 		{
@@ -31,7 +31,7 @@ int main(void)
 			{
 				if (execve(args[0], args, environ) == -1)
 				{
-					fprintf(stderr, "./hsh: command not found: %s\n", args[0]);
+					fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
 					exit(EXIT_FAILURE);
 				}
 			}
